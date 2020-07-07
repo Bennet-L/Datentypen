ff8d280d9af89cc15e243648d3c35c73080395c8
Bennet-L <67860734+Bennet-L@users.noreply.github.com>
2020-07-07 11:50:53 +0200

    Create Übung01.dart

---
+++ Übung01.dart
@@ -0,0 +1,15 @@
+main(){
+  int zahl = 0;
+  int dart = 11;
+  double kommazahl = 1.5;
+  double zdi = 2.225;
+  String projekt = "drei";
+  String projekt1 = "kaninchen";
+  
+  print(zahl);
+  print(dart);
+  print(kommazahl);
+  print(zdi);
+  print(projekt);
+  print(projekt1);
+}
